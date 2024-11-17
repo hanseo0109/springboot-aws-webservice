d69a41c72bc761dc37891dc6696853b43fa8fab8
hanseo0109 <hanseo0109@gmail.com>
2024-11-17 15:10:02 +0900

    Create post creation screen

--- src/main/java/com/self/web/dto/IndexController.java
+++ src/main/java/com/self/web/dto/IndexController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping("/posts/save")
     public String index(){
-        return "index";
+        return "posts-save";
     }
 }
