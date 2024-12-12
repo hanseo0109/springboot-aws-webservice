1ef66b9efecd96dfb19c496e5e9b40ab47e65814
hanseo0109 <hanseo0109@gmail.com>
2024-12-13 02:52:27 +0900

    2024.12.13 [ add-hs ] 세션 저장소로 데이터베이스 사용

--- src/main/java/com/self/web/dto/IndexController.java
+++ src/main/java/com/self/web/dto/IndexController.java
@@ -1,5 +1,6 @@
 package com.self.web.dto;
 
+import com.self.config.auth.LoginUser;
 import com.self.config.auth.dto.SessionUser;
 import com.self.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +17,22 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     /**
      * 여기서 model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
      * postsService.findAllDesc()로 가져온 결과를 -> posts로 index.mustache에 전달 함
      */
     @GetMapping("/")
-    public String index(Model model){
+    public String index(
+        Model model
+        , @LoginUser SessionUser user
+    ){
+//    public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
 
         // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         // 세션에 저장된 값이 있을때만 model에 userName 등록
         if( user != null){
